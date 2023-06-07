d93fdc30bf6d20a5b18b1d71f20f52bebbeef1af
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-06-07 19:15:20 +0900

    Show enableXsrfProtection config

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
 
+st.write(st._config.get_option("server.enableXsrfProtection"))
+
+
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     st.write(uploaded_file.name)
